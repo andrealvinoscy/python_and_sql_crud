bfeee3bb10f871c29cf80b0965c105e01a550b69
Andre Alvino <andrealvinoscy@gmail.com>
2026-03-05 14:14:26 -0300

    commit inicial - python MySQL conectado crud

---
+++ database/tabelacontatos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE contatos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100),
+    celular VARCHAR(20) NOT NULL,
+    telefone VARCHAR(20)
+);
---
+++ projetopy/crud01.py
@@ -0,0 +1,34 @@
+# Tentativa crud 01
+import mysql.connector
+
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="@Ndre123",
+    database="agenda_contatos"
+)
+
+cursor = conexao.cursor()
+
+def add_contatos():
+    nome_contato = input('Nome: ')
+    while nome_contato == "":
+        nome_contato = input('Nome é obrigatório. Digite novamente: ')
+
+    email_contato = input('E-mail: ')
+
+    celular_contato = input('Celular: ')
+    while celular_contato == "":
+        celular_contato = input('Celular é obrigatório. Digite novamente: ')
+
+    telefone_contato = input('Telefone: ')
+
+    sql = "INSERT INTO contatos (nome, email, celular, telefone) VALUES (%s, %s, %s, %s)"
+    valores = (nome_contato, email_contato, celular_contato, telefone_contato)
+
+    cursor.execute(sql, valores)
+    conexao.commit()
+
+    print("Contato adicionado com sucesso!")
+
+add_contatos()
